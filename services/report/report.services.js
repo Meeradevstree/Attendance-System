8e4eb652a17579416ef730460c65c17e53d1e132
Burhanuddin Rampura <burhanuddin.devstree@gmail.com>
2023-02-24 18:05:43 +0530

    df

--- services/report/report.services.js
+++ services/report/report.services.js
@@ -65,20 +65,21 @@ exports.list = async (reqQuery) => {
             const departmentData = await departmentAPI.find().lean()
             console.log('department : ======================',)
         }
+
     }
 
-    // returnData.total_counts = await reportModel.countDocuments(query).lean();
-    // returnData.total_pages = Math.ceil(returnData.total_counts / parseInt(limit));
-    // returnData.current_page = reqQuery.page ? parseInt(reqQuery.page) : 0;
+    returnData.total_counts = await reportModel.countDocuments(query).lean();
+    returnData.total_pages = Math.ceil(returnData.total_counts / parseInt(limit));
+    returnData.current_page = reqQuery.page ? parseInt(reqQuery.page) : 0;
 
-    // returnData.list = await reportModel.find(query).sort({})
-    //     .populate({ path: 'employeeName', select: ['first_name', 'last_name'] })
-    //     .populate({ path: 'projectName', select: 'projectName' })
-    //     // .populate({ path: 'projectManager', model: 'Project' , select:'projectManager', populate: { path: 'projectManager', model: 'Employee' , select:['first_name', 'last_name'] } })
-    //     // .populate({ path: 'teamLeader', model: 'Project' , select:'projectLeader', populate: { path: 'projectLeader', model: 'Employee' , select:['first_name', 'last_name'] } })
-    //     .skip(skip).limit(limit).lean();
+    returnData.list = await reportModel.find(query).sort({})
+        .populate({ path: 'employeeName', select: ['first_name', 'last_name'] })
+        .populate({ path: 'projectName', select: 'projectName' })
+        // .populate({ path: 'projectManager', model: 'Project' , select:'projectManager', populate: { path: 'projectManager', model: 'Employee' , select:['first_name', 'last_name'] } })
+        // .populate({ path: 'teamLeader', model: 'Project' , select:'projectLeader', populate: { path: 'projectLeader', model: 'Employee' , select:['first_name', 'last_name'] } })
+        .skip(skip).limit(limit).lean();
 
-    // return returnData;
+    return returnData;
 
 
 };
